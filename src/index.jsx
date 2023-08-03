7ebbb82a634be967e5d1d876bde3e3c61638c94a
Laura <laura.simonnet.pro@gmail.com>
2023-08-03 19:37:35 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Error from './components/Error'
+import Home from './pages/Home'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+
