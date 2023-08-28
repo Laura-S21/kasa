4635114bd9ee7ed54b2800a91c55a7161a000e3f
Laura <laura.simonnet.pro@gmail.com>
2023-08-28 17:45:03 +0200

    :feat: add error route from location page

--- src/pages/Location/index.jsx
+++ src/pages/Location/index.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { useParams, Navigate } from "react-router-dom"
 import { useLocationsById } from "../../hooks/logements"
 import Slideshow from "../../components/Slideshow"
 import redStar from "../../assets/red-star.png"
@@ -12,57 +12,64 @@ function Logement() {
     const redStars = Array(5).fill(redStar)
     const greyStars = Array(5).fill(greyStar)
 
+    if (!location) {
+        return (
+            <Navigate to="/error" replace={true} />
+        )
+    }
+
     return (
         <>
+            
             <Slideshow />
-                <div className="location-wrapper">
-                    <h1 className="location-wrapper__title">{location.title}</h1>
-                    <p className="location-wrapper__map">{location.location}</p>
-                    <div className="tags-wrapper">
-                        {location.tags.map((locationTag) => (
-                            <div className="tag-button"
-                                key={locationTag}
-                                >
-                                {locationTag}
-                            </div>
-                        ))}
-                    </div>
-                    <div className="host-wrapper">
-                            <p className="host-wrapper__name">{location.host.name}</p>
-                            <img className="host-wrapper__pic" src={location.host.picture} alt="Hôte" />
-                            
-                    </div>
-                    <div className="host-wrapper__rating">
-                        {redStars.slice(5-location.rating).map((redStar, index )=> {
-                            return <img className="host-wrapper__rating-star" src={redStar} alt="" key={"redStar"+index}/>
-                        })}
+            <div className="location-wrapper">
+                <h1 className="location-wrapper__title">{location.title}</h1>
+                <p className="location-wrapper__map">{location.location}</p>
+                <div className="tags-wrapper">
+                    {location.tags.map((locationTag) => (
+                        <div className="tag-button"
+                            key={locationTag}
+                            >
+                            {locationTag}
+                        </div>
+                    ))}
+                </div>
+                <div className="host-wrapper">
+                        <p className="host-wrapper__name">{location.host.name}</p>
+                        <img className="host-wrapper__pic" src={location.host.picture} alt="Hôte" />
+                        
+                </div>
+                <div className="host-wrapper__rating">
+                    {redStars.slice(5-location.rating).map((redStar, index )=> {
+                        return <img className="host-wrapper__rating-star" src={redStar} alt="" key={"redStar"+index}/>
+                    })}
 
-                        {greyStars.slice(location.rating).map((greyStar, index) => {
-                            return <img className="host-wrapper__rating-star" src={greyStar} alt="" key={"greyStar"+index}/>
-                        })}
-                    </div>
+                    {greyStars.slice(location.rating).map((greyStar, index) => {
+                        return <img className="host-wrapper__rating-star" src={greyStar} alt="" key={"greyStar"+index}/>
+                    })}
                 </div>
-                <div className="location-collapse-wrapper">
-                    <div className="location-collapse" >
-                        <Collapse
-                            key={location.id}
-                            title={"Description"}
-                            content={location.description}
-                        />
-                    </div>
-                    <div className="location-collapse">
-                        <Collapse
-                            key={location.id}
-                            title={"Équipements"}
-                            content={<ul className="equipment-list">
-                                {location.equipments.map((equipment) => (
-                                <li key={equipment}>
-                                    {equipment}
-                               </li> ))}
-                            </ul>}
-                        />
-                    </div>
+            </div>
+            <div className="location-collapse-wrapper">
+                <div className="location-collapse" >
+                    <Collapse
+                        key={location.id}
+                        title={"Description"}
+                        content={location.description}
+                    />
                 </div>
+                <div className="location-collapse">
+                    <Collapse
+                        key={location.id}
+                        title={"Équipements"}
+                        content={<ul className="equipment-list">
+                            {location.equipments.map((equipment) => (
+                            <li key={equipment}>
+                                {equipment}
+                            </li> ))}
+                        </ul>}
+                    />
+                </div>
+            </div>
         </>
     )
 }
